33b6f32d4191cc59e88ca05809ffc6284df47748
Christopher Boillot <boillot@gmail.com>
2026-06-13 20:04:57 -0500

    Style startup chooser with exact Montserrat Black

--- src/BattletechCharacterCreator.App/App.xaml.cs
+++ src/BattletechCharacterCreator.App/App.xaml.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace BattletechCharacterCreator.App;
@@ -38,6 +41,41 @@ public partial class App : Application
             return;
         }
 
+        var captureArgument = e.Args.FirstOrDefault(
+            argument => argument.StartsWith("--capture-start=", StringComparison.Ordinal));
+        if (captureArgument is not null)
+        {
+            var outputPath = Path.GetFullPath(captureArgument["--capture-start=".Length..]);
+            var start = new StartWindow();
+            start.Loaded += (_, _) => start.Dispatcher.BeginInvoke(
+                DispatcherPriority.ApplicationIdle,
+                () =>
+                {
+                    CaptureWindow(start, outputPath);
+                    start.Close();
+                    Shutdown(0);
+                });
+            start.Show();
+            return;
+        }
+
         new StartWindow().Show();
+    }
+
+    private static void CaptureWindow(Window window, string outputPath)
+    {
+        var dpi = VisualTreeHelper.GetDpi(window);
+        var bitmap = new RenderTargetBitmap(
+            (int)Math.Ceiling(window.ActualWidth * dpi.DpiScaleX),
+            (int)Math.Ceiling(window.ActualHeight * dpi.DpiScaleY),
+            dpi.PixelsPerInchX,
+            dpi.PixelsPerInchY,
+            PixelFormats.Pbgra32);
+        bitmap.Render(window);
+
+        var encoder = new PngBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(bitmap));
+        using var output = File.Create(outputPath);
+        encoder.Save(output);
     }
 }
